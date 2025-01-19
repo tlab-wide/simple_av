b185d4e58d4579c2ebbc83e3572d6d59a3f30238
unknown <hrhhemati@gmail.com>
2025-01-19 18:51:32 +0330

    adding new msg and topic for portal node

--- simple_av/simple_av/portal.py
+++ simple_av/simple_av/portal.py
@@ -4,6 +4,7 @@ from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Point
 import numpy as np
 import math
+from simple_av_msgs.msg import Portal
 
 
 class portal(Node):
@@ -17,6 +18,7 @@ class portal(Node):
             self.pose_callback,
             10
         )
+        self.portal_publisher = self.create_publisher(Portal, 'simple_av/portal', 10)
         self.pose = PoseStamped()
         self.initial_position = Point()
         self.isInitialPoseSampled = False
@@ -47,19 +49,25 @@ class portal(Node):
             print(self.initial_position)
             print(self.current_pose)
             print(distance_to_init)
-            if self.calculate_distance(self.current_pose, self.initial_position) < 10.0 and not self.isPortalReached:
+            if self.calculate_distance(self.current_pose, self.initial_position) < 5.0 and not self.isPortalReached:
                 print("Portal Detected")
                 self.isPortalReached = True
                 self.initial_pose = self.current_pose
-            if self.isPortalReached:
-                self.isPortalReached = False
         self.last_pose = self.current_pose
+        self.publish_portal()
+                
 
     def portal_reset(self):
         if not self.isInitialPoseSampled:
             self.initial_pose()
         else:
             self.portal_detector()
+
+    def publish_portal(self):
+        portal_reset = Portal()
+        portal_reset.reset = self.isPortalReached
+        self.portal_publisher.publish(portal_reset)
+        self.isPortalReached = False
 
 
 def main(args=None):
