d3c77a0ab0a9c311f95d4e57f2590ee1feceb1ea
hamidrezaHemati <hrhhemati@gmail.com>
2025-09-17 15:13:48 +0330

    requirements modified

---
+++ ros_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y ros-humble-lanelet2 ros-humble-lanelet2-extension
+sudo apt install -y ros-humble-lanelet2-projection ros-humble-lanelet2-routing ros-humble-lanelet2-traffic-rules ros-humble-lanelet2-maps
+sudo apt install -y ros-humble-geographic-msgs
+sudo apt install -y librange-v3-dev
+sudo apt install -y libtinyxml2-dev ros-humble-tinyxml2-vendor
